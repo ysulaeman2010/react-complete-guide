2121b07362f2776506bd33474a52c730c1a4d8f8
ysulaeman2010 <ysulaeman2010@gmail.com>
2021-04-20 15:02:23 +0700

    course module one

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
